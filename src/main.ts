c00da6fa4d1f10b562c483d85803554440ec2e1f
imroc <dpl1024@163.com>
2022-11-07 01:26:34 +0800

    组件库 ant-design-vue

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
+import Antd from "ant-design-vue";
 import App from "./App.vue";
 import router from "./router";
 
@@ -8,8 +9,10 @@ const app = createApp(App);
 
 import "@/style/normalize.css";
 import "@/style/reset.css";
+import "ant-design-vue/dist/antd.css";
 
 app.use(createPinia());
 app.use(router);
+app.use(Antd);
 
 app.mount("#app");
